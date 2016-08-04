b086e6c5657de7a5cedb534f93dea821b9216ad8
thefreshduke <scottyshaw1@gmail.com>
2016-08-04 15:36:04 -0700

    Added IBOutlets and IBActions

--- GabrielStopwatch/ViewController.swift
+++ GabrielStopwatch/ViewController.swift
@@ -9,7 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var stopButton: UIButton!
+    
+    @IBAction func startTIme(sender: AnyObject) {
+        timeLabel.text = "start"
+    }
+    
+    @IBAction func stopButton(sender: AnyObject) {
+        timeLabel.text = "stop"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
